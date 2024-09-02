467db631ba3982496336d5925f26991dc0158a94
Rohan Rana <rohanrana33042@gmail.com>
2024-09-02 14:23:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
         }
         else{
             x.style.display = 'none';
-            showBtn.innerHTML = 'Show More'
+            showBtn.innerHTML = 'Double Click'
 
         }
 
